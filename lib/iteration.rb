9bd53f1b9067d273eb76d6b4296fe6cd9cbc1edb
Richard Bobo <richard.bobo@gmail.com>
2020-03-03 22:27:33 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,4 +1,21 @@
+array_1 = [['pepperoni', 'sausage'], ['green olives', 'green peppers'], ['onions', 'pineapple']]
+
 def join_ingredients(src)
+  
+  new_array = []
+  row_index = 0 
+  while row_index< src.count do 
+  a = src[row_index][0]
+  b = src[row_index][1]
+  c = "I love #{a} and #{b} on my pizza"
+  new_array << c 
+  row_index += 1 
+end 
+
+  new_array
+  
+   
+   
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
   #
   # Build a new Array that contains strings where each pair of foods is
@@ -7,12 +24,42 @@ def join_ingredients(src)
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
 end
+join_ingredients(array_1)
+
+
+
+
+array_2 = [[-1, -900], [10, 30], [0, 0], [14, 16 * -2.5], [Math.sin(1), 19]]
+
 
 def find_greater_pair(src)
+  
+  new_array = []
+  row_index = 0 
+  
+  while row_index < src.count do 
+    element_index = 0 
+    inner_results = []
+    
+    if src[row_index][0] > src[row_index][1]
+      inner_results << src[row_index][0]
+    else inner_results << src[row_index][1]
+  end
+    row_index += 1 
+  end 
+  new_array << inner_results
+  
+  new_array
+  
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
 end
+
+find_greater_pair(array_2)
+
+
+
 
 def total_even_pairs(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
